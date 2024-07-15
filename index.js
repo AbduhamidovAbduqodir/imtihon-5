683fc2f5477147f5c1506fc44850166d2d7fb990
AbduhamidovAbduqodir <abduhamidov20099@gmail.com>
2024-07-15 11:32:14 +0500

    imtihon5

---
+++ index.js
@@ -0,0 +1,6 @@
+
+var copy = document.querySelector(".slider").cloneNode(true);
+document.querySelector(".swiper").appendChild(copy);
+
+var copy = document.querySelector(".sliderr").cloneNode(true);
+document.querySelector(".swipper").appendChild(copy);
